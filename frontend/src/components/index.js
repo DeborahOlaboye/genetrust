dd88448add1b7af8288bd261eb9679fb9e11e7b6
DeborahOlaboye <deboraholaboye@gmail.com>
2026-03-12 07:29:25 +0100

    feat(upload): re-export all upload wizard components from main components barrel

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -4,3 +4,15 @@ export { default as SessionBanner } from './SessionBanner';
 export { default as TxBatchPanel } from './TxBatchPanel';
 export { default as WalletSelector } from './WalletSelector';
 export { default as WalletStatusIndicator } from './WalletStatusIndicator';
+
+// Upload wizard
+export {
+  DatasetUploadWizard,
+  FileDropZone,
+  FilePreview,
+  MetadataForm,
+  HashProgress,
+  UploadSuccess,
+  WizardStepBar,
+  WalletGate,
+} from './upload/index.js';
