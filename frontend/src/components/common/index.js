7c60e5e63ed71750d804da8f6cbcd26c0e68a417
DeborahOlaboye <deboraholaboye@gmail.com>
2026-04-28 07:26:06 +0100

    feat(common): add LoadingSpinner to barrel export

--- frontend/src/components/common/index.js
+++ frontend/src/components/common/index.js
@@ -2,3 +2,4 @@ export { default as Button } from './Button';
 export { default as ErrorBoundary } from './ErrorBoundary';
 export { default as ErrorDisplay } from './ErrorDisplay';
 export { LazyImage } from './LazyImage';
+export { default as LoadingSpinner } from './LoadingSpinner';
