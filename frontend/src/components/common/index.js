bfbdb349e171fe43a66bef67e33058115f1e6480
DeborahOlaboye <deboraholaboye@gmail.com>
2026-05-10 09:39:03 +0100

    feat(common): export RateLimitFeedback from common index

--- frontend/src/components/common/index.js
+++ frontend/src/components/common/index.js
@@ -17,3 +17,4 @@ export { default as FormSubmitOverlay } from './FormSubmitOverlay';
 export { default as ErrorRetryButton } from './ErrorRetryButton';
 export { default as WalletErrorBoundary } from './WalletErrorBoundary';
 export { default as PageLoadingGate } from './PageLoadingGate';
+export { RateLimitFeedback } from './RateLimitFeedback';
